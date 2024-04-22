d097d04ba49b87023710d208cc91b13d1af6a57b
firaol81 <firaol.baneta@southeasttech.edu>
2024-04-21 20:32:00 -0500

    Add project files.

---
+++ Bookstore/Models/DomainModels/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bookstore.Models
+{
+    public class User : IdentityUser
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
+    }
+}
---
+++ Bookstore/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Please enter your first name.")]
+        [Display(Name = "First Name")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name.")]
+        [Display(Name = "Last Name")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a username.")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
